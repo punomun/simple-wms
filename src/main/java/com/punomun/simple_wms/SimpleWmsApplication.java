fedf5908ed77be97b6b78a77e5448ec1d2717610
punomun <angelhazmato@gmail.com>
2025-12-03 13:59:45 +0100

    chore: Initial project setup

---
+++ src/main/java/com/punomun/simple_wms/SimpleWmsApplication.java
@@ -0,0 +1,13 @@
+package com.punomun.simple_wms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/punomun/simple_wms/SimpleWmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.punomun.simple_wms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
